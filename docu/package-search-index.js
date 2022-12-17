fd2d52c319fcb6de438b0b5cc6ef1ea9f0b03b9c
Janik Geist <janik.geist@googlemail.com>
2022-12-17 02:06:42 +0100

    Generated JavaDoc

---
+++ docu/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"actions.ReUnDo"},{"l":"actions.ReUnDo.cards"},{"l":"actions.speichern"},{"l":"actions.Zuege"},{"l":"adapter.primary"},{"l":"adapter.secondary"},{"l":"All Packages","u":"allpackages-index.html"},{"l":"entities"},{"l":"persistence"},{"l":"ports.inbound"},{"l":"ports.outbound"}];updateSearchResults();
